02771262a0dc491dc7bfcde929f157d8ba5bda12
CNLISIYIII <cnlisiyiii@163.com>
2021-10-26 14:04:18 +0800

    加动画

--- sandi/js/index.js
+++ sandi/js/index.js
@@ -51,3 +51,35 @@ $(document).on("click", ".deve-box .more", function () { //点击加载更多
     $(this).hide().prev().show()
 })
 
+
+/* 动画切入效果 */ 
+var isScroll = {
+    /*初始化*/
+    init: function (_el) {
+        this.start(_el);
+        $(window).on('scroll', function () {
+            isScroll.start(_el)
+        });
+    },
+    /*开始*/
+    start: function (_el) {
+        var self = this;
+        $(_el).each(function () {
+            var _self = $(this);
+            /*滚动高度*/
+            var isScrollTop = $(window).scrollTop();
+            /*滚动视度*/
+            var isWindowHeiget = $(window).height() * 0.9;
+            /**/
+            var _class = $(this).data('animation');
+            if (isScrollTop + isWindowHeiget > $(this).offset().top) {
+                _self.addClass(_class);
+            }
+        });
+    }
+}
+
+// 动画切入效果
+$(document).ready(function () {
+    isScroll.init('.divtest');
+})
